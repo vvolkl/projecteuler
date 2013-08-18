5aa9d68d217ce71d67ff3b7c2f76cd13d8e41434
vvolkl <valentin.voelkl@student.uibk.ac.at>
2013-08-18 21:16:04 +0200

    Made extensive use of pythons amazing libraries for problem 19, and premiered reduce() in the solution of problem 20.

--- e020.py
+++ e020.py
@@ -12,6 +12,9 @@ parser.add_argument('n', metavar='n', type=int, nargs='?', default=100,
 args = parser.parse_args()
 n=args.n
 
+import math
+digits = str(math.factorial(n))
+result = reduce(lambda x,y: x+y, [int(d) for d in digits])
 
 start = time.time()
 print 'result:'
