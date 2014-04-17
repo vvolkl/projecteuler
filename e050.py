1dfe7ebb6e03c922b061c688cec49d49319afafa
vvolkl <valentin.voelkl@student.uibk.ac.at>
2014-04-17 12:53:44 +0200

    easy solution - when you have the correct loop order

--- e050.py
+++ e050.py
@@ -7,7 +7,7 @@ import numpy as np
 import re
 import time
 parser = argparse.ArgumentParser(description=doc)
-parser.add_argument('n', metavar='n', type=int, nargs='?', default=100, 
+parser.add_argument('n', metavar='n', type=int, nargs='?', default=1000000, 
                    help='the main variable for our program')
 parser.add_argument("-v","--verbose", help="increase output verbosity",
                     action="store_true")
@@ -19,34 +19,61 @@ result = []
 ######## 
 
 from Primes import is_prime, sieve
-
+import gmpy
 __primes = np.array(sieve(1000000))
 
 def len_cons_primes(prim):
-    c  = sieve(prim)
-    r = []
-    for i in range(len(c)):
+    #c  = sieve(prim)
+    #print prim
+    c = __primes[__primes < prim/2 ]
+    r = [0]
+    
+    for i in range(int(len(c)))[::-1]:
         a = prim
-        j = 0
-        while a > 0:
-            a -= c[j+i]
-            j += 1
-        if a == 0:
-            r.append(j)
+        j = i
+        primsum = 0
+        while primsum < prim and j < len(c): 
+            #a -= c[j]
+            
+            primsum = np.sum(c[i:j])
+            j = j + 1
+            #print '\t',i,j,np.sum(c[i:j]),prim
+        if primsum == prim:
+            #if a < 1:
+            #    if a == 0:
+                r.append(j-i - 1)
+            #    continue
     return max(r)
 
-resultlen = 0
-for num in __primes[__primes > 100000]:
-    t = len_cons_primes(num)
-    if t > resultlen:
-        resultlen = t
-        result = num
-    if args.verbose:
-        print num, t, resultlen, result
-        
-        
-    
 
+def check_primes_for_sums():
+    resultlen = 0
+    print 'n', n
+    for num in __primes[(__primes < n) *(__primes > n/2)]:
+        t = len_cons_primes(num)
+        if t > resultlen:
+            resultlen = t
+            result = num
+        if args.verbose:
+            print num, t, resultlen, result
+        
+def check_sums_for_prime():    
+    result = 0
+    l = 0
+    for (prim,i) in zip(__primes[:20],range(20)):
+        s = 0
+         
+        j = i
+        while s + __primes[j] < n:
+            s += __primes[j]
+            if is_prime(s):
+                if j - i > l:
+                    l = j - i
+                    result = s
+                if args.verbose:
+                    print prim,result,s,l,j-i
+            j = j + 1
+    return result
 
 
 print 'result:'
