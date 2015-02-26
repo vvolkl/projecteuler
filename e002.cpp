15dafc6ab386cf1b639740df233021f325042169
vvolkl <valentin.voelkl@student.uibk.ac.at>
2015-02-26 17:50:15 +0100

    formatting

--- e002.cpp
+++ e002.cpp
@@ -1,28 +1,26 @@
 #include <iostream>
 using namespace std;
 
-int main(int argc, char *argv[]){
-
-int a; int b; 
-a = b = 1;
-int result = 0;
-while(a < 4000 * 1000){
-    cerr<<a<<"\t"<<b<<"\t"<<result<<endl;
-    a = a + b;
-    if(a % 2 == 0){
-        result += a;
+int main(int argc, char *argv[]) {
+    int a; int b; 
+    a = b = 1;
+    int result = 0;
+    while(a < 4000 * 1000) {
+        cerr<<a<<"\t"<<b<<"\t"<<result<<endl;
+        a = a + b;
+        if(a % 2 == 0) {
+            result += a;
+        }
+        b = a + b;
+        if(b % 2 == 0) {
+            result += b;
+        }
     }
-    b = a + b;
-    if(b % 2 == 0){
-        result += b;
+    // correct for last term if we went over the limit
+    if(b > 4000 * 1000 && b%2 == 0) {
+        result -= b;
     }
-}
-if(b > 4000 * 1000 && b %2 == 0){
-    result -=b;
-}
-cout<<"result: "<<result<<endl;
-cerr<<!(b % 2)<<"\t"<<!b%2<<endl;
-
-
-return 0;
+    cout<<"result: "<<result<<endl;
+    cerr<<!(b % 2)<<"\t"<<!b%2<<endl;
+    return 0;
 }
