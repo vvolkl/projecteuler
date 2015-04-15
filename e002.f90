892e6c62877e91986571bcfac44fa7ca15f393e1
vvolkl <valentin.voelkl@student.uibk.ac.at>
2015-04-15 16:57:46 +0200

    Finished last commit and added arguments.

--- e002.f90
+++ e002.f90
@@ -1,6 +1,10 @@
 
-recursive function fibsum(i,j, res) result(k)
-integer, intent(in) :: i,j,res
+recursive function fibsum(i,j,res,lim) result(k)
+! one step of the fibonacci series: add i and j to yield k
+! if the new term is even, it is added to 'result'
+! if k does not exceed lim, the function proceeds to call itself with the new
+! arguments
+integer, intent(in) :: i,j,res,lim
 integer :: k, res2
 ! fibonacci sequence
 k = i + j
@@ -13,12 +17,12 @@ end if
 ! where are you now
 write(*,*) k, res2
 ! recursive part: stop when the fibonacci term value exceeds 4 million
-if (k <= 4000000) then
+if (k <= lim) then
     ! call fibsum again with the larger two of the three values i,j,k
     if (i < j) then
-        k = fibsum(k,j,res2)
+        k = fibsum(k,j,res2,lim)
     else
-        k = fibsum(i,k,res2)
+        k = fibsum(i,k,res2,lim)
     end if
 else ! we are done, write result 
     write(*,*) res2
@@ -30,5 +34,5 @@ end function
 program e002
 implicit none
 integer :: fibsum
-write(*,*) fibsum(1,2,2)
+write(*,*) fibsum(1,2,2,4000000)
 end program
