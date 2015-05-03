e57772970a70424ca77a3af1da2fe59d9906029e
vvolkl <valentin.voelkl@student.uibk.ac.at>
2015-05-03 12:11:53 +0200

    delete repeated code

--- e006.py
+++ e006.py
@@ -1,19 +1,6 @@
-from __future__ import division
-doc = """
-
-Valentin Volkl """
-
-import argparse
 import numpy as np
 
-parser = argparse.ArgumentParser(description=doc)
-parser.add_argument('n', metavar='n', type=int, nargs='?', default=1000, 
-                   help='the main variable for our program')
-args = parser.parse_args()
-
-#print args.n
-n=args.n
-
-print -np.sum(np.arange(1,n+1)**2) + np.sum(np.arange(1,n+1))**2
-
+def solve(n=1000):
+    x = np.arange(1, n+1)
+    return  np.sum(x)**2 - np.sum(x**2) 
 
