6f9a10751b701744781b000610690782028dec31
vvolkl <valentin.voelkl@student.uibk.ac.at>
2015-05-03 12:07:09 +0200

    fixed whitespace to conform to pep8

--- e001.py
+++ e001.py
@@ -1,10 +1,10 @@
 import numpy as np
-import time
+
 
 def sumMultiples(n):
     d = np.arange(0, n, 1)
     return np.sum(d[(d % 3 == 0) + (d % 5 == 0)])
 
+
 def solve(n=1000):
     return sumMultiples(n)
-
