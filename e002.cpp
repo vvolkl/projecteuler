9b43d80106607d9db5660f1f45f6b4b8d48828ce
vvolkl <valentin.voelkl@student.uibk.ac.at>
2015-09-11 10:34:17 +0200

    remove 'using namespace std;' explictly insert std::

--- e002.cpp
+++ e002.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
-using namespace std;
 
 int main(int argc, char *argv[]) {
     int a; int b; 
     a = b = 1;
     int result = 0;
     while(a < 4000 * 1000) {
-        cerr<<a<<"\t"<<b<<"\t"<<result<<endl;
+        std::cerr<<a<<"\t"<<b<<"\t"<<result<<std::endl;
         a = a + b;
         if(a % 2 == 0) {
             result += a;
@@ -20,7 +19,7 @@ int main(int argc, char *argv[]) {
     if(b > 4000 * 1000 && b%2 == 0) {
         result -= b;
     }
-    cout<<"result: "<<result<<endl;
-    cerr<<!(b % 2)<<"\t"<<!b%2<<endl;
+    std::cout<<"result: "<<result<<std::endl;
+    std::cerr<<!(b % 2)<<"\t"<<!b%2<<std::endl;
     return 0;
 }
