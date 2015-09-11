8917feee9de1c5e903566aa01e1236e1533966e4
vvolkl <valentin.voelkl@student.uibk.ac.at>
2015-09-11 10:32:16 +0200

    remove 'using namespace std;' explicitly insert std::

--- e001.cpp
+++ e001.cpp
@@ -1,29 +1,28 @@
-// projecteuler.net
-// Valentin Volkl
-// TODO: implement improvement from projecteulerpdf
-// add all numbers divisible by 3 and 5 and subtract 
-// numbers divisible by 15
+/* projecteuler.net
+/ Valentin Volkl
+/ TODO: implement improvement from projecteulerpdf
+/ add all numbers divisible by 3 and 5 and subtract
+/ numbers divisible by 15
+*/
 
 #include <iostream>
 #include <string>
-using namespace std;
 
 int main(int argc, char *argv[]) {
-  long int N;
+  long int N, result;
   if ( argc > 1) {
-    N = atoi(argv[1]);
+    N = std::atoi(argv[1]);
   } else {
     N = 1000;
   }
-  cout<<"summing multiples of 3 and 5 up to "<<N<<"..."<<endl;
-  long int result = 0;
+  std::cout<<"summing multiples of 3 and 5 up to "<<N<<"..."<<std::endl;
   for(long int i=0; i < N; ++i) {
     if ( !(i % 3) || (i % 5)) {
       result += i;
     }
   }
-  cout<<"result: "<<endl;
-  cout<<result<<endl;
+  std::cout<<"result: "<<std::endl;
+  std::cout<<result<<std::endl;
   return 0;
 }
 
