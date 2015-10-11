1750d56d2e0871176de7b2f0199c746ecf275e03
vvolkl <valentin.voelkl@student.uibk.ac.at>
2015-10-11 15:26:58 +0200

    script to check answer. usage: ./check_answer <number> <answer>

---
+++ check_answer.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo 'Project Euler Problem  ' $1
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NOCOLOR='\033[0m'
+STRING_CORRECT=${GREEN}'CORRECT'${NOCOLOR}
+STRING_FALSE=${RED}'FALSE'${NOCOLOR}
+
+euler_solution=`grep Answer: project_euler.txt 2>/dev/null | sed ''$1'q;d'`
+euler_solution=${euler_solution/"Answer: "/}
+echo '    ' $euler_solution
+
+my_solution=`echo -n $2 | md5sum`
+my_solution=${my_solution/ -/}
+echo '    ' $my_solution
+
+printf 'The answer '
+printf $2 
+printf ' is '
+if [ $my_solution == $euler_solution ]
+then
+printf $STRING_CORRECT
+else
+printf $STRING_FALSE
+fi
+printf '\n'
