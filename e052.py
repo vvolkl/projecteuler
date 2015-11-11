94e8d29ae30090262647a5a587d5db9145f7e501
vali <vali@pcphsft82.cern.ch>
2015-11-11 22:30:54 +0100

    conform to new style

--- e052.py
+++ e052.py
@@ -1,49 +1,22 @@
-from __future__ import division
-doc = """
-
-Valentin Volkl """
-import argparse
-import numpy as np
-import re
-import time
-parser = argparse.ArgumentParser(description=doc)
-parser.add_argument('n', metavar='n', type=int, nargs='?', default=6, 
-                   help='the main variable for our program')
-parser.add_argument("-v","--verbose", help="increase output verbosity",
-                    action="store_true")
-args = parser.parse_args()
-n=args.n
-start = time.time()
-result = 0
-
-######## 
-i = 0
 
 def has_same_digits(a, b):
     for char in a:
         b = b.replace(char,'',1)
-        #print b
     return b == ''
 
 
-
-while not result:
-    j = 1
-    while has_same_digits(str(j*i),str((j+1)*i)) and j < n :
-        if j == n - 1:
-            result = i
-        if args.verbose:
-            print '\r %(i)5i \t %(j)1i' % locals(),
-        j = j + 1
-    i = i + 1
+def solve(n=6, verbose=False):
+    number = 0
+    result = 0
+    while not result:
+        multiplier = 1
+        while ( has_same_digits(str(number*multiplier), str((multiplier+1)*number)) 
+                and multiplier < n ):
+            if multiplier == n - 1:
+                result = number
+            multiplier += 1
+        if verbose:
+            print number, multiplier
+        number += 1
+    return result
      
-    
-
-
-
-
-print 'result:'
-print result 
-print 'elapsed time:'
-print time.time()-start
-
--- e055.py
+++ e055.py
@@ -1,50 +1,25 @@
-from __future__ import division
-doc = """
 
-Valentin Volkl """
-import argparse
-import numpy as np
-import re
-import time
-parser = argparse.ArgumentParser(description=doc)
-parser.add_argument('n', metavar='n', type=int, nargs='?', default=10000, 
-                   help='the main variable for our program')
-parser.add_argument("-v","--verbose", help="increase output verbosity",
-                    action="store_true")
-args = parser.parse_args()
-n=args.n
-start = time.time()
-result = 0
-
-######## 
-
-lycrels = []
-
-for i in range(n):
-    temp = i
-    j = 0
-    done = 0
-    if args.verbose:
-        print 'checking ', i
-    while j < 50  and not done:
-        if args.verbose:
-            print '\riteration ', j
-        temp = temp + int(str(temp)[::-1])
-        if str(temp)[::-1] == str(temp): 
-            done = 1
-            if args.verbose:
-                print 'found palindrome: ', temp
-        j = j + 1 
-    if done == 0:
-        result += 1
-        lycrels.append(i)
-        if args.verbose:
-            
-            print 'found lycrel:', i
-       
-
-print 'result:'
-print result 
-print 'elapsed time:'
-print time.time()-start
-
+def solve(n=10000, verbose=True):
+    result = 0
+    lycrels = []
+    for i in range(n):
+        temp = i
+        j = 0
+        done = 0
+        if verbose:
+            print 'checking ', i
+        while j < 50  and not done:
+            if verbose:
+                print '\riteration ', j
+            temp = temp + int(str(temp)[::-1])
+            if str(temp)[::-1] == str(temp): 
+                done = 1
+                if verbose:
+                    print 'found palindrome: ', temp
+            j = j + 1 
+        if done == 0:
+            result += 1
+            lycrels.append(i)
+            if verbose:
+                print 'found lycrel:', i
+    return result
