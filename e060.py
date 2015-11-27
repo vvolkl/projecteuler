1ee0dc2faeeec8ba9fc9d10ac17550b790950dc5
vali <vali@pcphsft82.cern.ch>
2015-11-27 11:43:55 +0100

    elucidate cryptic code

--- e060.py
+++ e060.py
@@ -5,25 +5,28 @@ import Primes
 
 
 def solve(n=1100, verbose=True):
-    p = Primes.sieve(n**2)
-    print 'generating checkTwoPrimeTable...'
-        #print e
-    checked = np.zeros((n+1,n+1))
+    primes = primehelpers.sieve(n**2)
+    # we only need the first n primes
+    primes = primes[:n + 1]
+    # generating checkTwoPrimeTable...
+    checked = np.zeros((n + 1, n + 1))
     for i in range(n):
-        for j in range(i,n):
-            checked[i,j] = primehelpers.checkTwoPrimes(p[i], p[j]) > 0
+        for j in range(i, n):
+            # checkTwoPrimes checks both concatenations
+            # so no need to call it for values j < i
+            checked[i, j] = primehelpers.checkTwoPrimes(primes[i], primes[j]) > 0
+    # c should be symmetric, but one half was not touched before
     c = checked + checked.T
-    p2 = p[:c.shape[0]]
-    for i in range(1,1100):
-        print i
-        for j in np.nonzero(c[i,:])[0]:
-            c1 = np.intersect1d(np.nonzero(c[i,:]), np.nonzero(c[j,:]))
-            for k in c1:
+    for i in range(1, 1100): # for each prime do
+        for j in np.nonzero(c[i, :])[0]: # go through its prime partners
+            # the next partner must show up in both previous lists
+            c1 = np.intersect1d(np.nonzero(c[i, :]), np.nonzero(c[j, :]))
+            for k in c1: # and repeat procedure until we find a set that is long enough
                 c2 = np.intersect1d(c1, np.nonzero(c[k, :]))
                 for l in c2:
                     c3 = np.intersect1d(c2, np.nonzero(c[l, :]))
                     for m in c3:
                         c4 = np.intersect1d(c3, np.nonzero(c[m, :]))
-                        print [p2[_o] for _o in [i,j,k,l,m]]
-                        return np.sum([p2[_o] for _o in [i,j,k,l,m]])
+                        # found 'em! return the sum of the corresponding primes
+                        return np.sum([primes[_o] for _o in [i, j, k, l, m]])
                 
