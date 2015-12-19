56b1cfc385ca1c096e06771533623d70f138f4f0
vvolkl <valentin.voelkl@student.uibk.ac.at>
2015-12-19 16:49:47 +0100

    switch to long int

--- e002.cpp
+++ e002.cpp
@@ -1,25 +1,28 @@
 #include <iostream>
 
-int main(int argc, char *argv[]) {
-    int a; int b; 
-    a = b = 1;
-    int result = 0;
-    while(a < 4000 * 1000) {
-        std::cerr<<a<<"\t"<<b<<"\t"<<result<<std::endl;
-        a = a + b;
-        if(a % 2 == 0) {
-            result += a;
-        }
-        b = a + b;
-        if(b % 2 == 0) {
-            result += b;
-        }
-    }
-    // correct for last term if we went over the limit
-    if(b > 4000 * 1000 && b%2 == 0) {
-        result -= b;
-    }
-    std::cout<<"result: "<<result<<std::endl;
-    std::cerr<<!(b % 2)<<"\t"<<!b%2<<std::endl;
-    return 0;
+int main() {
+  long int a = 1; 
+  long int b = 1; 
+  long int result = 0;
+  while(a < 4000000L ) {
+      std::cerr<<a<<"\t"<<b<<"\t"<<result<<std::endl;
+      a = a + b;
+      if(a % 2 == 0) {
+          result += a;
+      }
+      b = a + b;
+      if(b % 2 == 0) {
+          result += b;
+      }
+  }
+  // correct for last term if it is already over the limit
+  if(b > 4000000L && b % 2 == 0) {
+      result -= b;
+  }
+  std::cout<<"result: "<<result<<std::endl;
+  std::cerr<<!(b % 2)<<"\t"<<!b%2<<std::endl;
+
+
+  std::cout<<sizeof(result)<<std::endl;
+  return 0;
 }
