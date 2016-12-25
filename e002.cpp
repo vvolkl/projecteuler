c1cb3735cd8d9fe1bb4d662bb0f8c0b4699ad40b
Valentin Volkl <valentin.volkl@cern.ch>
2016-12-25 15:23:37 +0100

    move to new format

--- e002.cpp
+++ e002.cpp
@@ -1,24 +1,24 @@
-#include <iostream>
 
-int main() {
-    int a = 1; 
-    int b = 1; 
-    int result = 0;
-    while(a < 4000 * 1000) {
-        std::cerr<<a<<"\t"<<b<<"\t"<<result<<std::endl;
-        a = a + b;
-        if(a % 2 == 0) {
-            result += a;
-        }
-        b = a + b;
-        if(b % 2 == 0) {
-            result += b;
-        }
+int solve() {
+  int a = 1; //< first fibonacchi number
+  int b = 1; //< second fibonacchi number
+  int result = 0;
+  const int limit = 4000000;
+  while(a < limit) {
+    a = a + b;
+    if (a % 2 == 0) {
+      result += a;
     }
-    // correct for last term if it is already over the limit
-    if(b > 4000 * 1000 && b % 2 == 0) {
-        result -= b;
+    b = a + b;
+    if (b % 2 == 0) {
+      result += b;
     }
-    std::cout<<result<<std::endl;
-    return 0;
+  }
+  // correct for last term if it is already over the limit
+  if(b > limit && b % 2 == 0) {
+    result -= b;
+  }
+  return result;
 }
+
+#include "boilermain.cpp"
