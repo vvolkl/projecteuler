900566b653ede5f4281d11415eb4d672e03f30ed
Valentin Volkl <valentin.volkl@cern.ch>
2018-10-22 11:32:57 +0200

    uncomment debug comments

--- e001.f90
+++ e001.f90
@@ -3,10 +3,9 @@ program e001
 integer::res
 res = 0
 do i=1,999
-    !if ((MOD(i,5).EQ.0) .OR. (MOD(i,3).EQ.0)) then
-    !if (MOD(i,3).EQ.0) then
+    if ((MOD(i,5).EQ.0) .OR. (MOD(i,3).EQ.0)) then
     res = res + i
-    !end if
+    end if
 end do
 write(*,*)res
 stop
