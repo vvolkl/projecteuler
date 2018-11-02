377dd760de54ceddc819c530ea27cb85466c1d9f
Valentin Volkl <valentin.volkl@cern.ch>
2018-11-02 12:58:34 +0100

    Add unrolled version of first problem

---
+++ e001-unrolled.cpp
@@ -0,0 +1,17 @@
+
+#include "stdio.h"
+
+
+
+int main() {
+  int result = 0;
+  int  i = 0;
+  while (i < 990) {
+    result += 7 * i + 60;
+    i += 15;
+  }
+  result += 3983;
+  printf("%d\n",result);
+  return 0;
+}
+
